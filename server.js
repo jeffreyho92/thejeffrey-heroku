188d25281ffccb4daa44329234143011f0402709
JEFFREY <abc@email.com>
2016-12-24 07:14:51 +0000

    Done timestamp-api

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 var express = require('express')
+var moment = require('moment')
 var app = express()
 
+app.use(express.static(__dirname + '/public'));
+/*
 app.get('/', function(req, res){
     res.send('Hello World')
+})
+*/
+app.get('/:value', function(req, res){
+    var value = req.params.value
+    var object = {}
+    
+    if(/^\d{8,}$/.test(value)) {
+        value = moment(value, "X");
+    } else {
+        value = moment(value, "MMMM D, YYYY");
+    }
+    
+    if(value.isValid()){
+        object.unix = value.format("X")
+        object.natural = value.format("MMMM D, YYYY")
+    } else {
+        object.unix = null
+        object.natural = null
+    }
+
+    res.json(object)
 })
 
 app.listen(8080, function(){
